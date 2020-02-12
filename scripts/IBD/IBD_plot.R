fad2b506622783dc3d0244622c0171542d6c3f7b
jelmerp <jelmerpoelstra@gmail.com>
2020-02-12 16:52:48 -0500

    Edit axis annotation

--- scripts/IBD/IBD_plot.R
+++ scripts/IBD/IBD_plot.R
@@ -1,103 +1,71 @@
-#### GLOBAL SET-UP -------------------------------------------------------------
-## Packages:
+#### SET-UP --------------------------------------------------------------------
+## Settings:
+setID <- 'msp3proj.all.mac3.FS6' # TO DO: TRY FS7
+
+## Script with functions:
 library(here)
-suppressPackageStartupMessages(library(tidyverse))
-suppressPackageStartupMessages(library(viridis))
-suppressPackageStartupMessages(library(plotly))
-if(!dir.exists(output_dir)) dir.create(output_dir, recursive = TRUE)
+source(here('scripts/genomics_link/IBD/IBD_fun.R'))
+
+## Dirs/files:
+input_dir <- here('analyses/IBD/RDS/')
+output_dir <- here('analyses/IBD/plots/')
+plotbase <- paste0(output_dir, 'IBD_', setID, '_')
 
 ## Colors:
 my_cols <- RColorBrewer::brewer.pal(name = 'Set1', n = 3)[1:2]
 #my_cols <- colorblindr::palette_OkabeIto[1:2]
 
-## Dirs:
-input_dir <- here('analyses/IBD/RDS/')
-output_dir <- here('analyses/IBD/plots/')
+
+#### RUN -----------------------------------------------------------------------
+outfile_plot <- paste0(plotbase, 'comp.png')
+
+p_mitleh <- ibd_plot_wrap(setID, subsetID = 'mitleh', input_dir, output_dir,
+                          plotplotly = FALSE, save_plot = FALSE)
+p_inset <- ibd_plot_wrap(setID, subsetID = 'mitleh', input_dir, output_dir,
+                         plotplotly = FALSE, save_plot = FALSE, pointsize = 2)
+p_macsp3 <- ibd_plot_wrap(setID, subsetID = 'macsp3', input_dir, output_dir,
+                          plotplotly = FALSE, save_plot = FALSE)
+
+(pm <- p_macsp3 +
+    xlim(c(7, 13)) +
+    ylim(c(0, 1.3)) +
+    ggtitle(expression(paste(italic("macarthuri - M."), " sp. #3"))) +
+    theme(axis.title = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks.y = element_blank(),
+          legend.background = element_rect(color = 'grey30'),
+          plot.margin = margin(10, 0.2, 15, 0.5)))
+
+(pl <- p_mitleh +
+    xlim(c(7, 13)) + ylim(c(0, 1.3)) +
+    ggtitle(expression(italic("mittermeieri - lehilahytsara"))) +
+    guides(color = FALSE) +
+    theme(axis.title.x = element_blank(),
+          plot.margin = margin(10, 0.5, 15, 0.2)))
+
+(pl_inset <- p_inset +
+    guides(color = FALSE) +
+    theme(axis.title = element_blank(), axis.text = element_blank(),
+          axis.ticks = element_blank(), panel.grid = element_blank(),
+          panel.background = element_rect(fill = 'grey90', colour = 'grey10'),
+          plot.margin = margin(0, 0, 0, 0)))
+
+p <- pl + pm
+pl2 <- ggdraw() + draw_plot(p) +
+  draw_plot(pl_inset, x = 0.02, y = 0.48, width = 0.35, height = 0.35) +
+  draw_plot_label(c("A", "B"), x = c(0.06, 0.41), y = c(1, 1), size = 15) +
+  draw_plot_label("Geographic distance", x = 0.45, y = 0.085,
+                  hjust = 0.5, size = 16, fontface = "plain")
+ggsave(outfile_plot, width = 7.2, height = 3.5); system(paste('xdg-open', outfile_plot))
 
 
-#### FUNCTIONS -----------------------------------------------------------------
-# mytheme(): create ggplot theme
-mytheme <- function () {
-  theme_bw() %+replace%
-    theme(axis.title = element_text(size = 16),
-          axis.text = element_text(size = 16),
-          legend.title = element_text(face = 'bold', size = 14),
-          legend.text = element_text(size = 14))
-}
-theme_set(mytheme())
+#### RESULT --------------------------------------------------------------------
+## mitleh:
+# Statistic: Pearson's product-moment correlation
+# Value: 0.6929998
+# Significance: 0.002
 
-# dist2df(): transform an object of class 'dist' to a dataframe:
-dist2df <- function(dist, varname = 'distance') {
-  as.data.frame(as.matrix(dist)) %>%
-    rownames_to_column() %>%
-    rename(site1 = rowname) %>%
-    pivot_longer(cols = -site1, names_to = 'site2') %>%
-    filter(site1 != site2) %>%
-    rename(!!varname := value)
-}
-
-## Create tidy df:
-mantel2df <- function(mantel, lookup) {
-  Dgeo_df <- dist2df(mantel$Dgeo, varname = 'geo_dist')
-  Dgen_df <- dist2df(mantel$Dgen, varname = 'gen_dist')
-  dist_df <- merge(Dgeo_df, Dgen_df, by = c('site1', 'site2')) %>%
-    mutate(sp1 = lookup$sp[match(site1, lookup$site)],
-           sp2 = lookup$sp[match(site2, lookup$site)],
-           comparison = ifelse(sp1 == sp2, 'intraspecific', 'interspecific'))
-}
-
-## Create IBD plot:
-ibd_plot <- function(dist_df,
-                     show_plot = TRUE,
-                     save_plot = TRUE,
-                     outfile_plot = NULL) {
-  ## Plot as regular ggplot:
-  p <- ggplot(data = dist_df) +
-     geom_point(aes(x = geo_dist, y = gen_dist, color = comparison), size = 4) +
-     scale_color_manual(values = my_cols) +
-     labs(x = "Geographic distance", y = 'Genetic distance')
-
-  if(save_plot == TRUE) {
-    ggsave(outfile_plot, width = 6, height = 4)
-    system(paste('xdg-open', outfile_plot))
-  }
-
-  ## Create Plotly plot:
-  p <- p + aes(text = paste0(site1, '-', site2))
-  pl <- ggplotly(p)
-  if(show_plot == TRUE) print(pl)
-}
-
-## Wrapper:
-ibd_plot_wrap <- function(setID, subsetID) {
-
-  ## Files:
-  plotbase <- paste0(output_dir, setID, '_', subsetID)
-  outfile_plot <- paste0(plotbase, '.png')
-  mantel_RDS <- paste0(input_dir, setID, '_', subsetID, '_mantel.RDS')
-  stopifnot(file.exists(mantel_RDS))
-
-  ## Get mantel RDS:
-  mantel <- readRDS(mantel_RDS)
-  cat("## Statistic:", mantel$mantel$method, '\n')
-  cat("## Value:", mantel$mantel$statistic, '\n')
-  cat("## Significance:", mantel$mantel$signif, '\n')
-
-  ## Plot:
-  lookup <- mantel$lookup %>% select(species, site_short) %>% rename(site = site_short)
-  dist_df <- mantel2df(mantel, lookup)
-  ibd_plot(dist_df, outfile_plot = outfile_plot)
-
-}
-
-
-
-#### RUN -----------------------------------------------------------
-setID <- 'msp3proj.all.mac3.FS6' # TO DO: CHANGE TO FS7
-subsetID <- 'mitleh'
-ibd_plot_wrap(setID, subsetID)
-## Statistic: Pearson's product-moment correlation
-## Value: 0.6929998
-## Significance: 0.002
-
-
+## macsp3:
+# Statistic: Pearson's product-moment correlation
+# Value: 0.6929998
+# Significance: 0.002
--- scripts/IBD/IBD_run.R
+++ scripts/IBD/IBD_run.R
@@ -1,117 +1,35 @@
 #https://rdrr.io/cran/dartR/f/inst/doc/IntroTutorial_dartR.pdf
 
-#### SET-UP---------------------------------------------------------------------
+#### SET-UP --------------------------------------------------------------------
 ## Settings:
-setID <- 'msp3proj.all.mac3.FS6' # TO DO: CHANGE TO FS7
+setID <- 'msp3proj.all.mac3.FS6' #setID <- 'msp3proj.all.mac3.FS7'
+subsets <- list('mitleh' = c('mmit', 'mleh'),
+                'macsp3' = c('mmac', 'msp3'))
 
-## Packages:
+## Script with functions:
 library(here)
-suppressPackageStartupMessages(library(tidyverse))
-suppressPackageStartupMessages(library(vcfR))
-suppressPackageStartupMessages(library(adegenet))
-suppressPackageStartupMessages(library(dartR))
+source(here('scripts/genomics_link/IBD/IBD_fun.R'))
 
 ## Input files:
-# VCF:
 infile_vcf <- here('seqdata/vcf/', paste0(setID, '.vcf.gz'))
 
-# Metadata:
 infile_lookup <- here('metadata/radseq_metadata_link/lookup_IDshort.txt')
 infile_sites <- here('metadata/lemurs_metadata_link/lookup/sites_gps.txt')
 infile_pops <- here('metadata/lemurs_metadata_link/lookup/sites_pops.txt')
-
-# Check:
 stopifnot(file.exists(infile_lookup), file.exists(infile_sites),
-          file.exists(infile_pops), file.exists(infile_vcf))
+          file.exists(infile_pops))
 
 ## Output files:
-outdir_plots <- here('analyses/IBD/plots/')
-if(!dir.exists(outdir_plots)) dir.create(outdir_plots, recursive = TRUE)
 outdir_RDS <- here('analyses/IBD/RDS/')
+RDS_vcf <- paste0(outdir_RDS, setID, '_snps.RDS')
 if(!dir.exists(outdir_RDS)) dir.create(outdir_RDS, recursive = TRUE)
-out_RDS_base <- paste0(outdir_RDS, setID, '_')
 
 
-#### READ AND PREP DATA --------------------------------------------------------
-## Read VCF:
-snps_raw <- vcfR2genlight(read.vcfR(infile_vcf))
-snps_raw@ind.names <- substr(snps_raw@ind.names, 1, 7) # Long->short IDs
-saveRDS(snps_raw, paste0(out_RDS_base, 'snps_raw.RDS'))
+#### RUN MANTEL TEST -----------------------------------------------------------
+## Prep:
+lookup <- prep_metadata(setID)
+snps <- read_vcf(infile_vcf, lookup, RDS_vcf)
 
-## Read and process metadata:
-lookup_raw <- read.delim(infile_lookup, as.is = TRUE) %>%
-  select(ID, Sample_ID, species, sp, site)
-
-pops <- read.csv(infile_pops, as.is = TRUE) %>%
-  select(site, site_short, site_lump, sp, pop2)
-sites <- read.delim(infile_sites, as.is = TRUE) %>%
-  select(site, sp, lat, lon) %>%
-  merge(., pops, by = c('site', 'sp'), all.x = TRUE)
-
-# Subset to inds in VCF:
-stopifnot(all(snps_raw@ind.names %in% lookup_raw$ID)) # Check if all VCF inds are in lookup
-lookup_raw_sel <- lookup_raw %>% filter(ID %in% snps_raw@ind.names)
-
-# Include site GPS coords:
-stopifnot(all(lookup_raw_sel$site %in% sites$site)) # Check if all sites are in site-lookup
-lookup <- lookup_raw_sel %>%
-  merge(., sites, by = c('site', 'sp')) %>%
-  mutate(site_short = ifelse(is.na(site_short), site, site_short),
-         site_lump = ifelse(is.na(site_lump), site, site_lump),
-         pop2 = ifelse(is.na(pop2), sp, pop2)) %>%
-  arrange(match(ID, snps_raw@ind.names)) # Same order as in "snps" object!
-
-## Modify genlight object:
-(snps_raw@other$latlong <- lookup %>% select(lat, lon)) # Include lat-long in genlight object
-(snps_raw@pop <- lookup %>% pull(site_short) %>% factor()) # Include pop-info
-
-
-#### RUN IBD TEST --------------------------------------------------------------
-## Subset to specific species:
-subsetID <- 'mitleh'
-focal_sps <- c('mittermeieri', 'lehilahytsara')
-lookup_sel <- filter(lookup, species %in% focal_sps)
-inds_sel <- lookup_sel %>% pull(ID)
-snps_sel <- gl.keep.ind(snps, ind.list = inds_sel)
-
-## Do Mantel test at population level (uses FST):
-mantel <- gl.ibd(snps_sel) #[, 1:100]
-mantel$lookup <- lookup_sel
-saveRDS(mantel, paste0(out_RDS_base, subsetID, '.RDS'))
-
-
-
-
-
-#### FILTER A VCF --------------------------------------------------------------
-#snps <- new('genlight', as.matrix(snps)[keep.rows, ])
-
-#### TESTING -------------------------------------------------------------------
-# ordering <- levels(snps@pop)
-# latlon <- RgoogleMaps::geosphere_mercator(snps@other$latlong)
-# latlon <- apply(latlon, 2, function(a) tapply(a, pop(snps), mean, na.rm = T))
-# Dgeo <- as.dist(as.matrix(log(dist(latlon)))[ordering, ordering])
-# Dgen <- gl.dist.pop(snps[, 1:1000], method = "euclidean")
-# Dgen <- as.dist(as.matrix(Dgen)[ordering, ordering])
-#
-# ## Mantel test at individual level:
-# snps_sel_ind <- snps_sel
-# snps_sel_ind@pop <- factor(snps_sel_ind@ind.names)
-# aap <- gl.ibd(snps_sel_ind[, 1:100])
-#
-# ## "Manual" Mantel:
-# #gl.ibd
-# snps_sel_ind <- snps_sel
-# snps_sel_ind@pop <- factor(snps_sel_ind@ind.names)
-# ordering <- levels(snps_sel_ind@pop)
-# latlon <- RgoogleMaps::geosphere_mercator(snps_sel@other$latlong) +
-#   round(rnorm(length(inds_sel), mean = 5, sd = 10)) # Add random noise to avoid same GPS
-# #latlon <- snps_sel@other$latlong
-# Dgeo <- log(dist(latlon))
-# str(Dgeo)
-# Dgeo <- as.dist(as.matrix(Dgeo)[ordering, ordering])
-# Dgen <- gl.dist.pop(snps_sel_ind, method = "euclidean")
-# Dgen <- as.dist(as.matrix(Dgen)[ordering, ordering])
-#
-# dim(latlon)
-# length(snps_sel_ind@ind.names)
+## Run for subsets:
+mantel_wrap(subsets['macsp3'], snps, lookup)
+#mantel_wrap(subsets['mitleh'], snps, lookup)
